f22ac5e798a662931d7e46e0070fec37fde9a00e
Kirill Nemtsov <KN17022004@yandex.ru>
2023-07-05 13:45:34 +0500

    fullstack fix

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
-# Подключение статических файлов (CSS, JS, изображения)
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-# Подключение шаблонов Jinja2
 templates = Jinja2Templates(directory="templates")
 
-# Главная страница
+@app.get("/")
+async def index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 @app.get("/")
 async def index(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
