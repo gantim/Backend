31bbd2fcc690f5a3be0ce06a7ed1130e222f1c3a
Kirill Nemtsov <KN17022004@yandex.ru>
2023-07-05 13:43:36 +0500

    fullstack test

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
-@app.get('/')
-def index():
-    return {'message': 'Добро пожаловать в API!'}
+# Подключение статических файлов (CSS, JS, изображения)
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+# Подключение шаблонов Jinja2
+templates = Jinja2Templates(directory="templates")
+
+# Главная страница
+@app.get("/")
+async def index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
